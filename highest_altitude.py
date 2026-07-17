a78040a3fe1688042d64e28399976a070fb89e6a
Sam John Paul <samjohnpauloff@gmail.com>
2026-07-17 12:44:04 +0530

    Add files via upload

---
+++ highest_altitude.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestAltitude(self, gain):
+        altitude = 0
+        highest = 0
+
+        for i in gain:
+            altitude += i
+            highest = max(highest, altitude)
+
+        return highest
